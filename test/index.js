fb54ed259c3af98f0366d112d5a9c7668c40c68f
Ivan Dobrianov <gitpub16-f70@dobrianov.net>
2017-04-25 00:05:10 -0400

    Better reg test setup

--- test/index.js
+++ test/index.js
@@ -2,17 +2,25 @@ let should = require('chai').should();
 let footinch = require('../index');
 let parse = footinch.parse;
 
+const cases = {
+  '1'             : 1,
+  '5 1 1/2'       : 5 + (1+1/2)/12,
+  '5 1/2'         : 5 + 1/2/12,
+  '5 1/2"'        : (5+1/2)/12,
+  // '5-6'           : 5+6/12,
+  '1 "'           : NaN
+}
+
 describe('#parse', function() {
-  it('Parses: 5', function() {
-    parse('5').should.equal(5)
-  });
-  it('Parses: 5 1 1/2', function() {
-    parse('5 1 1/2').should.equal(5 + (1+1/2)/12)
-  });
-  it('Parses: 5 1/2', function() {
-    parse('5 1/2').should.equal(5 + .5/12)
-  });
-  it('Parses: 5 1/2"', function() {
-    parse('5 1/2"').should.equal((5 + .5)/12)
+  Object.keys(cases).forEach(function(key) {
+    let data = cases[key];
+    it(key + ' --> ' + data, function() {
+      if (isNaN(data)){
+        parse(key).should.be.NaN;
+      }
+      else {
+        parse(key).should.equal(data);
+      }
+    });
   });
 });
