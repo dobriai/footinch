63cfc4cf1ec51525862424965ee522d4135266fc
Ivan Dobrianov <gitpub16-f70@dobrianov.net>
2017-05-15 23:14:42 -0400

    Regression coverage for format
    Also renamed the old, parse test to parse.js

---
+++ test/format.js
@@ -0,0 +1,25 @@
+let should = require('chai').should();
+let footinch = require('../index'), format = footinch.format;
+
+describe('#format:IMPERIAL', function () {
+  let xx = format.FT.to.FT.DEC(2);
+  const cases = {
+    "xx(12.123456)" : "12.12 ft",
+    "format.FT.to.FT.DEC(2, ' feet')(12.5)" : "12.50 feet",
+    "format.FT.to.FT.IN.DEC(3)(12 + 2.12345/12)" : "12' 2.123\"",
+    "format.FT.to.FT.IN.FRAC(8)(12 + (2 + 5/8)/12)" : "12' 2 5/8\"",
+    "format.FT.to.FT.IN.FRAC(8)(12 + (2 + 21/32)/12)" : "12' 2 5/8\"",
+    "format.FT.to.FT.IN.FRAC(8)(12 + (2 + 22/32)/12)" : "12' 2 3/4\"",
+    "format.FT.to.FT.IN.FRAC(8)(12 + (2 + 23/32)/12)" : "12' 2 3/4\"",
+    "format.FT.to.FT.IN.FRAC(8)(-1*(12 + (2 + 23/32)/12))" : "-12' 2 3/4\"",
+    "format.FT.to.FT.IN.FRAC(8, [' ft ', ' in'])(-1*(12 + (2 + 23/32)/12))" : "-12 ft 2 3/4 in"
+  };
+
+  Object.keys(cases).forEach(function (key) {
+    // console.log(str + " --> " + eval(str));
+    const data = cases[key];
+    it(key + ' --> ' + data, function() {
+      eval(key).should.equal(data);
+    });
+  });
+});
