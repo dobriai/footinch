92bbec04b4a12f1f4fb61640f0a11c64f21a3093
Ivan Dobrianov <gitpub16-f70@dobrianov.net>
2017-05-15 21:58:35 -0400

    Split parser in a separate file
    No functionality changes. Just move the parsing logic into a separate
    file - formatting coming soon.
    
    Also tweak the demo.js file to show how to use minimal inclusion.

--- misc/demo.js
+++ misc/demo.js
@@ -1,8 +1,12 @@
 // Quick demo:
-let footinch = require('../index');
+// OPTION A - get everything:
+// let footinch = require('../index'), parse = footinch.parse;
+//
+// OPTION B - get only the parser:
+let parse = require('../lib/parse');
 
 ['1', '1\'', '1"', '1m', '1-1/2', '1 1/2', '.01e2', '-1e-3km'].forEach(str => {
-  let numF = footinch.parse.F(str);
-  let numM = footinch.parse.M(str);
+  let numF = parse.F(str);
+  let numM = parse.M(str);
   console.log(str + " --> " + numF + ' feet = ' + numM + ' meters');
 });
