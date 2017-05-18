064a078f9ac7fcb0dd7d24f1924aa67c62457265
Ivan Dobrianov <ivan.dobrianov@autodesk.com>
2017-05-18 13:17:57 -0400

    Bugfix ft + dec inches, e.g. 3' 2.5"
    We were failing to handle the case of feet plus decimal inches. For
    example:
    3 2.5
    was not being parse properly.

--- misc/demo-parse.js
+++ misc/demo-parse.js
@@ -5,7 +5,7 @@
 // OPTION B - get only the parser:
 let parse = require('../lib/parse');
 
-['1', '1\'', '1"', '1m', '1-1/2', '1 1/2', '.01e2', '-1e-3km'].forEach(str => {
+['1', '1\'', '1"', '1m', '1-1/2', '1 1/2', '.01e2', '-1e-3km', '3 6.5', '2\' 3.5"', '3 6.'].forEach(str => {
   let numF = parse.F(str);
   let numM = parse.M(str);
   console.log(str + " --> " + numF + ' feet = ' + numM + ' meters');
