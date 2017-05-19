fc370040f88cce7c22d811bd8219ff9085936e41
Ivan Dobrianov <gitpub16-f70@dobrianov.net>
2017-05-18 22:54:06 -0400

    ESlint - skip quotes checking
    Have not made up my mind which style to use yet :-) Or, I just don't
    care :-)

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,10 @@ module.exports = {
             "error",
             "unix"
         ],
-        "quotes": [
-            "error",
-            "single"
-        ],
+        // "quotes": [
+        //     "error",
+        //     "single"
+        // ],
         "semi": [
             "error",
             "always"
