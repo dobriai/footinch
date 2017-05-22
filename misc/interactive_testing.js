dd6c3ad60328326cb2ef32c7af0a0279f17dd172
Ivan Dobrianov <ivan.dobrianov@autodesk.com>
2017-05-22 14:10:32 -0400

    Bugfix in a demo file

--- misc/interactive_testing.js
+++ misc/interactive_testing.js
@@ -3,6 +3,6 @@ let footinch = require('../index');
 
 let str = '1 7/8';
 while (true) {  //eslint-disable-line no-constant-condition
-  let num = footinch.parseF(str);
+  let num = footinch.parse.F(str);
   console.log(str + " = " + num);
 }
